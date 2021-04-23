c899dd103c2ddf6980ae600c67b0adce57514ca2
walopes <walopes23@gmail.com>
2021-04-22 22:39:58 -0300

    Added Welcome page; App.js changed

---
+++ plantmanager/src/pages/Welcome.tsx
@@ -0,0 +1,75 @@
+import React from 'react';
+import {StyleSheet, SafeAreaView, Text, Image, TouchableOpacity} from 'react-native';
+
+import wateringImg from '../assets/watering.png';
+import colors from '../styles/colors';
+
+export function Welcome(){
+    return(
+        <SafeAreaView style={styles.container}>
+            <Text style={styles.title}>
+                Gerencie {'\n'}
+                suas plantas {'\n'}
+                de forma fácil
+            </Text>
+            <Image source={wateringImg} style={styles.image} />
+
+            <Text style={styles.subtitle}>
+                Não esqueça mais de regar as suas plantas.
+                Nós cuidamos delas para você sepre que precisar.
+            </Text>
+
+            <TouchableOpacity 
+                style={styles.button} 
+                activeOpacity={0.7}
+            >
+                <Text style={styles.buttonText}>
+                    >
+                </Text>
+            </TouchableOpacity>
+
+        </SafeAreaView>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'space-between'
+    },
+    title: {
+        fontSize: 32,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        color: colors.heading,
+        marginTop: 38
+    },
+
+    subtitle: {
+        textAlign: 'center',
+        fontSize: 18,
+        paddingHorizontal: 20, 
+        color: colors.heading
+    },
+
+    button: {
+        backgroundColor: colors.green,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 16,
+        marginBottom: 10,
+        height: 56,
+        width: 56
+    },
+
+    image: {
+        width: 292,
+        height: 284
+    },
+
+    buttonText: {
+        color: colors.white,
+        fontSize: 24
+    }
+});
