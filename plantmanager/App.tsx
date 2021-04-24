80ff62149dcce9c20332784bed2b4dc42f72dfaa
walopes <walopes23@gmail.com>
2021-04-24 20:12:12 -0300

    added Confirmation page

--- plantmanager/App.tsx
+++ plantmanager/App.tsx
@@ -7,7 +7,7 @@ import {
   Jost_600SemiBold
 } from '@expo-google-fonts/jost'
 
-import { UserIdentification } from './src/pages/UserIdentification';
+import { Confirmation } from './src/pages/Confirmation';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -20,6 +20,6 @@ export default function App() {
     return <AppLoading />
 
   return (
-    <UserIdentification />
+    <Confirmation />
   )
 };
