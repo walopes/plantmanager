208541ac0a2592fc993daa513584707349418c35
walopes <walopes23@gmail.com>
2021-04-25 16:48:38 -0300

    updated fontText value for Button component

--- plantmanager/src/components/Button.tsx
+++ plantmanager/src/components/Button.tsx
@@ -36,7 +36,7 @@ const styles = StyleSheet.create({
         alignItems: 'center'
     },
     text: {
-        fontSize: 32,
+        fontSize: 18,
         color: colors.white,
         fontFamily: fonts.heading
     }
